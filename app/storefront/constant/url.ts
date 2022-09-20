539ed7203172132d2ee70e04a9353040f048f6b3
Peter Mbanugo <peter@foobar.agency>
2022-09-20 16:36:12 +0200

    update CLUSTER_IP keyword

--- app/storefront/constant/url.ts
+++ app/storefront/constant/url.ts
@@ -1,11 +1,13 @@
 // TODO: configure these values from .env files
 export const productDetail =
-  'https://product-detail.default.CLUSTER_IP.sslip.io';
-export const productList = 'https://product-list.default.CLUSTER_IP.sslip.io';
+  'https://product-detail.default.CLUSTER_EXTERNAL_IP.sslip.io';
+export const productList =
+  'https://product-list.default.CLUSTER_EXTERNAL_IP.sslip.io';
 
 export const productCategory =
-  'https://product-category.default.CLUSTER_IP.sslip.io';
-export const cartGet = 'https://cart-get.default.CLUSTER_IP.sslip.io';
-export const cartUpdate = 'https://cart-update.default.CLUSTER_IP.sslip.io';
-export const order = 'https://order.default.CLUSTER_IP.sslip.io';
-export const checkout = 'https://checkout.default.CLUSTER_IP.sslip.io';
+  'https://product-category.default.CLUSTER_EXTERNAL_IP.sslip.io';
+export const cartGet = 'https://cart-get.default.CLUSTER_EXTERNAL_IP.sslip.io';
+export const cartUpdate =
+  'https://cart-update.default.CLUSTER_EXTERNAL_IP.sslip.io';
+export const order = 'https://order.default.CLUSTER_EXTERNAL_IP.sslip.io';
+export const checkout = 'https://checkout.default.CLUSTER_EXTERNAL_IP.sslip.io';
